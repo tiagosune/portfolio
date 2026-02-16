fc605e14b63b96c534a119aa9f03fa5abd3e9891
Tiago <tiagosune1@hotmail.com>
2026-02-16 14:51:37 -0300

    feat: adicionar seções About, Projects e Skills no componente Home
    - Criar os componentes About, Projects e Skills com suas respectivas estruturas (HTML, CSS e TypeScript).
    - Atualizar o componente Home para importar e exibir as novas seções.
    - Implementar testes unitários básicos para os novos componentes.

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {Projects} from './sections/projects/projects';
+import {Skills} from './sections/skills/skills';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Hero, About, Projects, Skills],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
