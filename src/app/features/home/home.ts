19c300a8eae7f9a664092395909cbf7954665b98
Tiago <tiagosune1@hotmail.com>
2026-02-16 14:51:59 -0300

    feat: adicionar seção Hero ao componente Home
    - Criar o componente Hero com estrutura (HTML, CSS e TypeScript).
    - Atualizar o componente Home para importar e exibir a nova seção Hero.

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Hero} from './sections/hero/hero';
+import {About} from './sections/about/about';
 import {Projects} from './sections/projects/projects';
 import {Skills} from './sections/skills/skills';
 
---
+++ src/app/features/home/sections/hero/hero.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {Button} from '../../../../shared/components/button/button'
+
+@Component({
+  selector: 'app-hero',
+  imports: [Button],
+  templateUrl: './hero.html',
+  styleUrl: './hero.css',
+})
+export class Hero {
+
+}
