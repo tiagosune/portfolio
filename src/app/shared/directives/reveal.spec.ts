1c7a54fbcb3e5411e368e78939587ab70d1b5b23
Tiago <tiagosune1@hotmail.com>
2026-02-19 01:21:00 -0300

    style: ajustar layout
    - Reduzir o espaçamento vertical da seção "Projetos" de `py-32` para `py-28`.
    - Adicionar comportamento `scroll-behavior: smooth` ao elemento `html` no arquivo `styles.css`.

---
+++ src/app/shared/directives/reveal.spec.ts
@@ -0,0 +1,8 @@
+import { Reveal } from './reveal';
+
+describe('Reveal', () => {
+  it('should create an instance', () => {
+    const directive = new Reveal();
+    expect(directive).toBeTruthy();
+  });
+});
