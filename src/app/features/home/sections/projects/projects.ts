8bcc1223b0bce7447860bbc7fcfbe51884651a84
Tiago <tiagosune1@hotmail.com>
2026-02-19 01:22:53 -0300

    feat(projects): refatorar seção para uso do componente ProjectCard
    - Reestruturar código para usar componente reutilizável `ProjectCard` na exibição de projetos.
    - Implementar loop dinâmico para renderizar projetos a partir de uma lista no TypeScript.
    - Integrar `RevealDirective` para animações de entrada nos projetos.
    - Ajustar layout com grid responsivo para melhorar a exibição em diferentes resoluções.

--- src/app/features/home/sections/projects/projects.ts
+++ src/app/features/home/sections/projects/projects.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { Project } from '../../../../shared/models/project.model';
+import { ProjectCard } from '../../../../shared/components/project-card/project-card';
+import {RevealDirective} from '../../../../shared/directives/reveal';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [ProjectCard, RevealDirective],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
 export class Projects {
+
+  projects: Project[] = [
+    {
+      title: "Meu portfólio",
+      description: "Portfólio pessoal",
+      tech: ["Angular", "TypeScript", "Tailwind CSS"],
+      image: "images/portfolio.png",
+      demo: "https://tiagosune.com",
+      github: "https://github.com/tiagosune/portfolio"
+    },
+    {
+      title: "QrArt",
+      description: "SaaS de geração de QR Codes personalizados e dinâmicos integrado com Stripe para pagamentos.",
+      tech: ["Java", "Spring Boot", "React"],
+      image: "images/qrart.png",
+      demo: "https://qrart.com.br",
+      github: "https://github.com/tiagosune/QRArt"
+    },
+    {
+      title: "Site institucional Incopel",
+      description: "Desenvolvi do zero o site institucional da empresa mineradora Incopel, com o objetivo de melhorar o\n" +
+        "            posicionamento online da empresa.",
+      tech: ["HTML", "CSS", "JavaScript"],
+      image: "images/incopel.png",
+      demo: "https://tiagosune.github.io/Incopel"
+    },
+    {
+      title: "MedType",
+      description: "Sistema web para criar, armazenar e organizar laudos médicos de ultrassonografia.",
+      tech: ["Java", "Spring Boot", "React"],
+      image: "images/medtype.png",
+      github: "https://github.com/tiagosune/MedType"
+    }
+  ];
 
 }
