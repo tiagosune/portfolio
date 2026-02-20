32b2c2d31115fba2915697404c245e4d0b11884c
Tiago <tiagosune1@hotmail.com>
2026-02-20 01:47:16 -0300

    feat(contact): adicionar funcionalidade de copiar e-mail para a área de transferência
    - Substituir link de e-mail por botão interativo no template.
    - Implementar lógica com `signal` para copiar e-mail ao clicar no botão.
    - Adicionar feedback visual com mensagem de sucesso ao copiar e-mail.

--- src/app/features/home/sections/contact/contact.ts
+++ src/app/features/home/sections/contact/contact.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {RevealDirective} from '../../../../shared/directives/reveal';
 
 @Component({
   selector: 'app-contact',
-  imports: [
-    RevealDirective
-  ],
+  imports: [RevealDirective],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
+
+  email = 'tiagosune1@hotmail.com';
+  copied = signal(false);
+
+  async copyEmail() {
+    try {
+      await navigator.clipboard.writeText(this.email);
+      this.copied.set(true);
+
+      setTimeout(() => {
+        this.copied.set(false);
+      }, 2000);
+
+    } catch (err) {
+      console.error('Erro ao copiar email', err);
+    }
+  }
 
 }
