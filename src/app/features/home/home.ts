3602665a41c92bb48b29296158f3fb6ffe47bc9f
Tiago <tiagosune1@hotmail.com>
2026-02-20 01:32:31 -0300

    feat(home): integrar ScrollService e implementar registro de seções
    - Adicionar injeção de dependência do `ScrollService` no componente Home.
    - Implementar `AfterViewInit` para registrar seções dinamicamente com o serviço.

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,6 +3,8 @@ import {Hero} from './sections/hero/hero';
 import {About} from './sections/about/about';
 import {Projects} from './sections/projects/projects';
 import {Skills} from './sections/skills/skills';
+import {AfterViewInit} from '@angular/core';
+import {ScrollService} from '../../core/services/scroll.service';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,18 @@ import {Skills} from './sections/skills/skills';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements AfterViewInit {
+
+  constructor(private scrollService: ScrollService) {}
+
+  ngAfterViewInit() {
+    this.scrollService.registerSections([
+      'home',
+      'about',
+      'projects',
+      'skills',
+      'contact'
+    ]);
+  }
 
 }
